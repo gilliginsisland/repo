e6857ff4d4c7d1a39c46d97737f854e54c3f039b
Gil Broochian <gil@broochian.com>
2016-11-03 13:31:43 -0400

    / ‘repoprep.py’

--- repoprep.py
+++ repoprep.py
@@ -44,7 +44,6 @@ def gethashes(file):
 	md5 = hashlib.md5()
 	sha1 = hashlib.sha1()
 	sha256 = hashlib.sha256()
-	sha1 = hashlib.sha1()
 
 	with open(file, 'rb') as f:
 		for data in iter(lambda: f.read(65536), ''):
