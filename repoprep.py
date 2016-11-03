0ae3e492a74d91db3c9357078a0ceaa912bb6092
Gil Broochian <gil@broochian.com>
2016-11-03 12:28:55 -0400

    / ‘repoprep.py’

--- repoprep.py
+++ repoprep.py
@@ -5,6 +5,8 @@ def mkdirp(directory):
 	if not os.path.isdir(directory):
 		os.makedirs(directory)
 
+def
+
 repopath = os.path.dirname(sys.argv[0])
 with open(os.path.join(repopath,'repoprep.json'), 'r') as f:
 	conf = json.load(f)
@@ -12,8 +14,7 @@ with open(os.path.join(repopath,'repoprep.json'), 'r') as f:
 distsdir = os.path.join(repopath,'dists')
 
 for dist in conf["dists"]:
-	distdir = os.path.join(distsdir, dist['name'])
 	for component in dist["components"]:
 		for arch in dist['architectures']:
-			archdir = os.path.join(distdir, component, 'binary-' + arch)
+			archdir = os.path.join(dist['name'], component, 'binary-' + arch)
 			mkdirp(archdir)
