4dd77aa4952d8971cd2dea7dfaee0beceaf67f04
Gil Broochian <gil@broochian.com>
2016-11-03 12:58:16 -0400

    / ‘repoprep.py’

--- repoprep.py
+++ repoprep.py
@@ -5,14 +5,21 @@ def mkdirp(directory):
 	if not os.path.isdir(directory):
 		os.makedirs(directory)
 
-def scanpackages(repo, dist, component, arch):
-	dpkgargs = [
-		'dpkg-scanpackages',
-		'--arch',
-		arch,
-		os.path.join('pool', dist, component)
-	]
-	return subprocess.check_output(dpkgargs, cwd=repo)
+def scanpackages(repo, dist, component, architectures):
+	for arch in architectures:
+		## call dpkg-scanpackages to get package lists
+		dpkgargs = [
+			'dpkg-scanpackages',
+			'--arch',
+			arch,
+			os.path.join('pool', dist, component)
+		]
+		packages = subprocess.check_output(dpkgargs, cwd=repo)
+
+		## make directory to save the packages file in
+		packagespath = os.path.join(repo, dist, component, 'binary-' + arch)
+		mkdirp(archdir)
+
 
 repopath = os.path.dirname(sys.argv[0])
 with open(os.path.join(repopath,'repoprep.json'), 'r') as f:
