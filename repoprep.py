7b159326d56404be33c7d572bed167995b5ae34d
Gil Broochian <gil@broochian.com>
2016-11-03 12:08:07 -0400

    / ‘repoprep.py’

--- repoprep.py
+++ repoprep.py
@@ -3,4 +3,7 @@ import sys, os, json
 
 repopath = os.path.dirname(sys.argv[0])
 with open(os.path.join(repopath,'repoprep.json'), 'r') as f:
-	print(json.load(f))
+	conf = json.load(f)
+
+for dist in conf["dists"]:
+	print(dist)
